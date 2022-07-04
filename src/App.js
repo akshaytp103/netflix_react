bd4ccc9258eff7a9a79897411c6a6cea10c85c28
Akshay tp <akshaytp@gmail.com>
2022-07-05 04:40:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+import Banner from './Componects/Banner/Banner';
+import NavBar from './Componects/NavBar/NavBar';
+import RowPost from './Componects/RowPost/RowPost';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+export const API_KEY ="41b4e9be530cba8e31250f0a99705111";
+export const imageUrl='https://image.tmdb.org/t/p/original'
